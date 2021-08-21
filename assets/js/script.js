9db8f95cbdabbba8b488bd09991dd58b8673712b
PaulThomasWI <87770976+PaulThomasWI@users.noreply.github.com>
2021-08-21 16:42:52 -0500

    building form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
 var formElement  = document.querySelector("#task-form");
 var listElement = document.querySelector("#tasks-to-do");
 
-var handleTask = function (event) {
+var taskHandler = function (event) {
     event.preventDefault();
 
-    console.log("Entered handleTask");
+    var taskNameInput  = document.querySelector("input[name='task-name']").value;
+    var taskTypeSelect = document.querySelector("select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput
+        , type: taskTypeSelect
+    }
+
+    createTaskElement(taskDataObj);
+}
+
+var createTaskElement = function(taskDataObj) {
     listItemElement = document.createElement("li");
     listItemElement.className = "task-item";
-    listItemElement.textContent = "This is a new task!";
+
+    var divElement = document.createElement("div");
+    divElement.className = "task-info";
+    divElement.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+    listItemElement.appendChild(divElement);
     listElement.appendChild(listItemElement);
 }
 
-formElement.addEventListener("submit", handleTask);
+formElement.addEventListener("submit", taskHandler);
