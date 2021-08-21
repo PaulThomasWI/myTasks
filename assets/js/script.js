11b92ef8f27f000b23922973db52510cf8fa5734
PaulThomasWI <87770976+PaulThomasWI@users.noreply.github.com>
2021-08-21 15:22:37 -0500

    building add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var butElement  = document.querySelector("#save-task");
+var listElement = document.querySelector("#tasks-to-do");
+
+var handleTask = function () {
+    listItemElement = document.createElement("li");
+    listItemElement.className = "task-item";
+    listItemElement.textContent = "This is a new task!";
+    listElement.appendChild(listItemElement);
+}
+
+butElement.addEventListener("click", handleTask);
